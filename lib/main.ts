38cd8e9b8538dc6cc27bb206bb09b9a1d2f1f566
craftbox <bartoszdudzinski18@gmail.com>
2024-05-29 18:40:29 +0200

    added schema type export

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,7 @@
 import validate from "./validate"
 import { TypeValidationError, SchemaError, DynamicSchemaError } from "./Errors"
+import { TypeSchema } from "./types/schemaTypes"
 
 export { validate, TypeValidationError, SchemaError, DynamicSchemaError }
+
+export declare type Schema = TypeSchema
